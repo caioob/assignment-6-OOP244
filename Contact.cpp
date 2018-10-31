c29c4474fa9b0ed5e65d126dc01f023b34aea712
caioob <41481398+caioob@users.noreply.github.com>
2018-10-30 22:04:26 -0400

    Add files via upload

--- Contact.cpp
+++ Contact.cpp
@@ -1,57 +1,85 @@
-#include <iostream>
 #include "Contact.h"
+#include <iostream>
+#include <cstring>
 using namespace std;
 namespace sict{
+    void Contact::clearSeparator(){
+        countryCode = 0;
+        areaCode = 0;
+        fullNumber = 0;
+        firstPart = 0;
+        secondPart = 0;
+        //cout << "(+" << countryCode << ") " << areaCode << " " << phoneNumber[1] << "-" << phoneNumber[2] << endl;
+    }
+    void Contact::separator (long long phones_){
+        countryCode = phones_ /10000000000;
+        areaCode = (phones_%10000000000)/10000000;
+        fullNumber = phones_%10000000;
+        firstPart = fullNumber/10000;
+        secondPart = fullNumber%10000;
+    }
+
     Contact::Contact(){
         Name[0] = '\0';
         numberOfPhones = 0;
+        phones = nullptr;
     }
-    Contact::Contact(char Name_, double phones_, int numberOfPohnes_){
-        if (phones_ > 11001000000 && Name_ != '\0' && numberOfPhones_ > 0){
-            Name = Name_;
-            numberOfPhones_ = numberOfPhones;
-        }
-        for(int i = 0; i <= numberOfPhones_; i++){
-            int coutryCode, areaCode, phoneNumber;
-            countryCode = phones_ /10000000000;
-            areaCode = (phones_%10000000000)/10000000;
-            phoneNumber = areaCode%10000000;
-            if (coutryCode > 0 && countryCode < 100 && areaCode > 99 && areaCode < 1000 && phoneNumber > 999999 && phoneNumber < 10000000){
-                phones = new double[numberOfPohnes_];
+    Contact::Contact(char* Name_, long long* phones_, int numberOfPhones_){
+        long long validPhones [50];
+        clearSeparator();
+        Name[0] = '\0';
+        phones = nullptr;
+        numberOfPhones = 0;
+        for(int i = 1; i <= numberOfPhones_; i++){
+            if(phones_[i] > 10000000000  && phones_[i] < 999999999999){
+                separator(phones_[i]);
+            }
+            if (countryCode > 0 && countryCode < 100 && areaCode > 99 && areaCode < 1000 && fullNumber > 999999 && fullNumber < 10000000){
+                numberOfPhones = numberOfPhones + 1;
+                validPhones[numberOfPhones] = phones_[i];
+                clearSeparator();
             }
         }
+        if(Name_ != '\0' || Name_ != nullptr){
+            strncpy(Name, Name_, 20);
+        }
+        //copying valid number to the current object
+        if (numberOfPhones > 0 ){
+            int index = numberOfPhones;
+            phones = new long long [index];
+            for(int i = 0; i <= numberOfPhones; i++){
+                phones[i] = validPhones[i];
+            }
+            //numberOfPhones = validPhones;
+        }
+
     }
     Contact::~Contact(){
         delete [] phones;
-        //phones = nullptr;
+        phones = nullptr;
     }
     bool Contact::isEmpty() const{
-        if (Name[0] == '\0'){
-            return false;
-        }
-        else{
+        if (Name[0] == '\0' ){
             return true;
         }
+        else{
+            return false;
+        }
     }
-    int Contact::separator(double phone_){
-        int inter, area, number;
-        inter = phone_ /10000000000
-
 
     void Contact::display(){
-        int number[2];//used for separating the area code an phone numbers
-        if(Name[0] == '\0'){
+        if(isEmpty() == true){
             cout << "Empty contact!" << endl;
         }
-        else{
+        else if (isEmpty() != true){
             cout << Name << endl;
-            for (int i = 0; i <= numberOfPhones; i++){
-                cout << "(+" << phones[i]/10000000000 << ")";
-                number[0] = (phones[i]/10000000000) * 10000000000;
-                number[1] = phones[i] - number[0];
-                cout <<  number[1] / 100000000 << " ";
-                number[2] = (number[1]/100000000) * 100000000;
-                number =
+            for (int i = 1; i <= numberOfPhones; i++){
+                separator(phones[i]);
+                cout << "(+" << countryCode << ") " << areaCode << " " << firstPart << "-";
+                cout.width(4);
+                cout.fill('0');
+                cout<< secondPart << endl;
+
             }
         }
     }
--- Contact.h
+++ Contact.h
@@ -1,23 +1,23 @@
 #ifndef CONTACT_H
 #define CONTACT_H
-#define MAX_CHAR 20
 
 using namespace std;
 
 namespace sict{
-
+    const int MAX_CHAR = 20;
     class Contact{
         char Name[MAX_CHAR + 1];
-        double *phones;
+        long long *phones;
         int numberOfPhones;
-
+        int countryCode, areaCode, firstPart, secondPart, fullNumber;
     public:
         Contact();
-        Contact(char,double,int);
+        Contact(char*,long long*,int);
         ~Contact();
         bool isEmpty() const;
-        void display() const;
-
+        void display();// needed to make this function with write permissions to void separator(double)
+        void separator (long long);
+        void clearSeparator();
     };
 }
 #endif
